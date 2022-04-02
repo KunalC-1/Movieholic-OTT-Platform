f735a07635e1aecc3669d4b477d6ab15b61ed196
Kunal Chaudhari <chaudharikunals02@gmail.com>
2022-04-02 14:33:49 +0530

    Addition: added navbar and partial homepage

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import "./navbar.scss";
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    window.onscroll = () => {
+        setIsScrolled(window.scrollY === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+    console.log(isScrolled);
+    return (
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+            <div className="container">
+                <div className="left">
+                    <img
+                        src="https://i.ibb.co/s3wKbrc/movieholic.png"
+                        alt="movieholic"
+                    />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <Search className="icon" />
+                    <span>KID</span>
+                    <Notifications className="icon" />
+                    <img
+                        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                        alt=""
+                    />
+                    <div className="profile">
+                        <ArrowDropDown className="icon" />
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
