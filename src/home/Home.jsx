b206ca3c6d9a0c49ed31b43e3c097833f98db142
Kunal Chaudhari <chaudharikunals02@gmail.com>
2022-04-03 11:25:50 +0530

    Addition: featured movies list component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,14 +1,11 @@
 import Navbar from "../component/navbar/Navbar";
+import Featured from "../component/featured/Featured";
 import "./home.scss";
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <img
-                width="100%"
-                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-            />
+            <Featured type="movie" />
         </div>
     );
 };
