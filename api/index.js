9c7ed7f7b2b165b90b6ead090a79c33e527e7cf2
Nishant Badgujar <nsbadgujar2001@gmail.com>
2022-04-06 12:43:26 +0530

    Added till API Movie CRUD Operations

--- api/index.js
+++ api/index.js
@@ -2,17 +2,27 @@ const express = require("express");
 app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        // useCreateIndex: true,
     })
     .then(() => console.log("Database Connection Successful!!!"))
     .catch((err) => {
         console.error(err);
     });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+
 app.listen(5000, () => {
     console.log("Listening on Port 5000");
 });
