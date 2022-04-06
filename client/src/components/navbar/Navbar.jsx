f67c53cca557a2b67fc7c476e1abc3beb01fbd9f
Kunal Chaudhari <chaudharikunals02@gmail.com>
2022-04-06 21:39:52 +0530

    Integrated front end and back end

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
+
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const { dispatch } = useContext(AuthContext);
+
     window.onscroll = () => {
-        setIsScrolled(window.scrollY === 0 ? false : true);
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
         return () => (window.onscroll = null);
     };
-    console.log(isScrolled);
     return (
         <div className={isScrolled ? "navbar scrolled" : "navbar"}>
             <div className="container">
@@ -16,9 +21,15 @@ const Navbar = () => {
                         src="https://i.ibb.co/s3wKbrc/movieholic.png"
                         alt="movieholic"
                     />
-                    <span>Homepage</span>
-                    <span>Series</span>
-                    <span>Movies</span>
+                    <Link to="/" className="link">
+                        <span>Homepage</span>
+                    </Link>
+                    <Link to="/series" className="link">
+                        <span className="navbarmainLinks">Series</span>
+                    </Link>
+                    <Link to="/movies" className="link">
+                        <span className="navbarmainLinks">Movies</span>
+                    </Link>
                     <span>New and Popular</span>
                     <span>My List</span>
                 </div>
@@ -34,7 +45,9 @@ const Navbar = () => {
                         <ArrowDropDown className="icon" />
                         <div className="options">
                             <span>Settings</span>
-                            <span>Logout</span>
+                            <span onClick={() => dispatch(logout())}>
+                                Logout
+                            </span>
                         </div>
                     </div>
                 </div>
